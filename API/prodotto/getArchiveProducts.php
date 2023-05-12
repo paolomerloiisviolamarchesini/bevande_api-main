d5af40384dd3ce2ee70268c9f4ad75eba4b1d88b
paolomerloiisviolamarchesini <122516969+paolomerloiisviolamarchesini@users.noreply.github.com>
2023-05-12 13:08:08 +0200

    getArchiveProducts.php

--- API/prodotto/getArchiveProducts.php
+++ API/prodotto/getArchiveProducts.php
@@ -1,25 +1,9 @@
 <?php
 require __DIR__ . '/../../MODEL/prodotto.php';
-require __DIR__ . '/../../COMMON/connect.php';
+header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json; charset=UTF-8");
 
-$db = new Database();
-$conn = $db->connect();
-$prodotto = new Prodotto($conn);
+$prodotto = new Prodotto();
 $result = $prodotto->getArchiveProducts();
 
-$products = array();
-for ($i = 0; $i < (count($result)); $i++) {
-    $product = array(
-        "id" =>  $result[$i]["id"],
-        "id_categoria" => $result[$i]["id_categoria"],
-        "nome" => $result[$i]["nome"],
-        "descrizione" => $result[$i]["descrizione"],
-        "quantita" => $result[$i]["quantita"],
-        "prezzo" => $result[$i]["prezzo"],
-        "active" => $result[$i] ["active"]
-    );
-    array_push($products, $product);
-}
-
-echo json_encode($products);
+echo json_encode($result);
